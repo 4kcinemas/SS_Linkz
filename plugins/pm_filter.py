8fb7a4e1cc3bce89c0c66d6d5bf5fc608a9ff6e4
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-02 11:38:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -641,7 +641,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"📂[{get_size(file.file_size)}] 🎥{file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
