a1d7ab2a5da16122e65a5c3cdddc8ecc5beb966c
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-03-02 11:08:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/4kcinemas/SS_Linkz.git /SS_Linkz
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /SS_Linkz
 fi
-cd /EvaMaria
+cd /SS_Linkz
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Netflix....🔥"
 python3 bot.py
